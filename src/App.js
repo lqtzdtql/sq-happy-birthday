ee0065dbbcbcc4cc42feec9bfc03bf1892d584ce
liuqiton <liuqitong.123@bytedance.com>
2022-11-15 03:22:33 +0800

    feat: 增加黑洞

--- src/App.js
+++ src/App.js
@@ -1,60 +1,14 @@
-import React, { useEffect, useRef } from "react";
-import { Star } from './util/start' 
+import React from "react";
 import './App.css';
+import StarrySky from './components/starrySky'
+import BlackHole from "./components/blackHole";
 
 function App() {
-  const canvas = useRef(null);
-  const stars = [];
-  const maxStars = 1100; // 星星数量
-
-  const initCanvas = () => {
-    const ctx = canvas.current.getContext('2d');
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    console.log(w,h)
-    const canvas2 = document.createElement("canvas");
-    const ctx2 = canvas2.getContext("2d");
-    canvas2.width = 100;
-    canvas2.height = 100;
-    const half = canvas2.width / 2;
-    const gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-    gradient2.addColorStop(0.025, "#CCC");
-    gradient2.addColorStop(0.1, "hsl(217, 61%, 33%)");
-    gradient2.addColorStop(0.25, "hsl(217, 64%, 6%)");
-    gradient2.addColorStop(1, "transparent");
-    ctx2.fillStyle = gradient2;
-    ctx2.beginPath();
-    ctx2.arc(half, half, half, 0, Math.PI * 2);
-    ctx2.fill();
-    for (var i = 1; i < maxStars; i++) {
-      const star = new Star({w, h}, ctx, canvas2);
-      stars[i] = star;
-    }
-    function animation() {
-      ctx.globalCompositeOperation = "source-over";
-      ctx.globalAlpha = 0.8; //尾巴
-      ctx.fillStyle = "hsla(217, 64%, 6%, 2)";
-      ctx.fillRect(0, 0, w, h);
-
-      ctx.globalCompositeOperation = "lighter";
-      for (let i = 1; i < stars.length; i++) {
-        stars[i].draw();
-      }
-
-      window.requestAnimationFrame(animation);
-    }
-    animation();
-  }
-
-  useEffect(()=>{
-    initCanvas();
-  },[]);
-
-  
 
   return (
     <div className="App">
-      <canvas id="canvas" width={ window.innerWidth} height={ window.innerHeight }  ref={canvas}></canvas>
+      <StarrySky />
+      <BlackHole />
     </div>
   );
 }
---
+++ src/components/blackHole/index.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useRef } from "react";
+import './index.css';
+
+function BlackHole() {
+  const canvas = useRef(null);
+  const elements = 1000; // more the merrier
+  const shiftmod = 1/6; // modifier for the x&y distance between each element: DEFAULT=1
+  const angle = 37; // degree to rotate the canvas between each element
+  const scale =  5; // number of elements that would fit in the available height
+  const colorstep = 0.75; // how quickly to adjust the color each frame
+  const coloralpha = 0.50; // alpha to render elements in
+  const colorcap = 255; // maximum color brightness
+  const rotation = 1/10; // how much to rotate the canvas each frame
+  const clearScreenAlpha = 0.90; // alpha value for the screen erase each frame
+  const zoom = 1.50; // zoom level
+  const triheight = 0.10; 
+
+  function start(){
+    init();
+    tick();
+  }
+
+  const colors = new Array(elements);
+  const increments = new Array(elements);
+  let size;
+  let shift;
+
+  function init(){
+    const targetheight = canvas.current.height*zoom; 
+    shift = targetheight/elements*shiftmod;
+    size = Math.max(1, targetheight/scale);
+    
+    for( let i = 0; i < elements; i++ ){
+      increments[i] = 0;
+      colors[i] = Math.round(colorcap - colorcap*i/elements);
+    }
+  }
+
+  let framesRendered = 0;
+
+  function draw() {
+    if (canvas.current.getContext) {
+      const context = canvas.current.getContext("2d");
+      
+      context.setTransform(1, 0, 0, 1, 0, 0);
+      
+      clearCanvas(context);
+      
+      // set the origin
+      context.translate(canvas.current.width/2, canvas.current.height/2);
+      
+      // rotate the canvas based on the frame
+      context.rotate(2 * Math.PI/360 *framesRendered*rotation);
+      
+      // draw them all
+      for( var i = 0; i < elements; i++ )
+      {
+        if( getRandom(1, 5) === 1 )
+        {
+           if( increments[i] !== 0 )
+            increments[i] = 0;
+           else
+            increments[i] = getRandom(-1, 1) * colorstep;
+        }
+        
+        colors[i] += increments[i];
+        if( colors[i] < 0 )
+        {
+            colors[i] = 0;
+            increments[i] = colorstep;
+        }
+        else if( colors[i] > colorcap )
+        {
+            colors[i] = colorcap;
+            increments[i] = -colorstep;
+        }
+        
+        context.fillStyle = "rgba(" + 
+          Math.floor(colors[i]) + "," +
+          Math.floor(colors[i]) + "," +
+          "0, " + coloralpha + ")";
+        
+        context.rotate(2 * Math.PI/360 * angle);
+        
+        context.beginPath();
+        context.moveTo(i*shift - size/2, size*triheight + i*shift);
+        context.lineTo(i*shift, i*shift);
+        context.lineTo(i*shift + size/2, size*triheight + i*shift);
+        context.closePath();
+        context.fill();
+      }
+      
+      framesRendered++;
+    }
+  }
+
+  function tick(){
+      // draw a frame
+      draw();
+      
+      // trigger the timer for the next frame...
+      window.requestAnimationFrame(tick);
+  }
+
+  function clearCanvas(context){
+    context.fillStyle = "rgba(0, 0, 0, " + clearScreenAlpha + " )";
+    context.fillRect (0, 0, canvas.width, canvas.height);
+  }
+
+  function getRandom (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+
+  useEffect(()=>{
+    start();
+  },[]);
+
+  
+
+  return (
+    <div className="App">
+      <canvas id="blackHole" width={ 300 } height={ 300 }  ref={canvas}></canvas>
+    </div>
+  );
+}
+
+export default BlackHole;
